b694c33eb0ff323fe86fe388bde995df81c9b64f
Huda Yousif <hjdoski@gmail.com>
2019-10-07 21:07:31 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_languge(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
